6b2015c94a6e07300833ef34dc5fc7602ea9f0f3
Ardelean Calin <ardelean.calin@outlook.com>
2018-10-21 17:59:22 +0300

    Dockerized functions, as well.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,27 @@
 # Use Node 8 as base
 FROM node:8
+
+# Bottom-up approach for node_packages installation, so that Docker caches
+# the files and doesn't execute npm install unless something changed
+WORKDIR /app/functions
+# copy both 'package.json' and 'package-lock.json' (if available)
+COPY ./functions/package*.json ./
+# Install the dependencies for 'functions'
+RUN npm install
+# copy project files and folders to the functions directory
+COPY ./functions/. .
+
 # Set the /app folder as the current workdir
 WORKDIR /app
 # copy both 'package.json' and 'package-lock.json' (if available)
 COPY package*.json ./
+# Install the dependencies for 'improved-pancake'
 RUN npm install
 # copy project files and folders to the current working directory (i.e. 'app' folder)
 COPY . .
+
+# Set the firebase admin credentials for cloud functions
+RUN export GOOGLE_APPLICATION_CREDENTIALS="functions/improved-pancake-credentials.json"
 # Expose port 8080 to the world
 EXPOSE 8080
 # Default command
--- functions/index.js
+++ functions/index.js
@@ -23,6 +23,8 @@ exports.userCreated = functions.auth.user().onCreate(async event => {
 
   const items = await getItems();
 
+  // Create a toReview structure with the courses the newly created
+  // user has yet to review.
   const toReview = Object.keys(items).reduce((prev, item) => {
     prev[item] =
       items[item].dateStart < new Date().getTime()
@@ -44,9 +46,7 @@ exports.userCreated = functions.auth.user().onCreate(async event => {
 });
 
 exports.assignReviews = functions.https.onRequest(async (req, resp) => {
-  const endDate = new Date();
-  const startDate = new Date().setDate(endDate.getDate() - 14); // two weeks ago
-
+  const currentTime = new Date();
   const items = await getItems();
   const users = await firebase
     .database()
@@ -56,40 +56,61 @@ exports.assignReviews = functions.https.onRequest(async (req, resp) => {
   await users.forEach(async snapshot => {
     const userID = snapshot.key;
     const toReview = snapshot.child("toReview");
+
     let toReviewClone = { ...toReview.val() };
-
     toReview.forEach(snap => {
-      const id = snap.key;
-      const val = snap.val();
-      const itemStartDate = items[id] && items[id].dateStart;
-
-      // Only consider items which are in the last 2 weeks
-      if (
-        itemStartDate &&
-        itemStartDate > startDate &&
-        itemStartDate <= endDate
-      ) {
-        toReviewClone[id] =
-          toReview.child(id).val() == "not_assigned"
-            ? "assigned"
-            : toReviewClone[id];
-        // eslint-disable-next-line
-        console.log(`Assigned: ${id} for ${userID}`);
-      } else {
-        toReviewClone[id] =
-          toReview.child(id).val() == "assigned"
-            ? "not_assigned"
-            : toReviewClone[id];
-        // eslint-disable-next-line
-        console.log(`Unassigned: ${id} for ${userID}`);
-      }
+      console.log(snap.val());
     });
-
-    await firebase
-      .database()
-      .ref(`users/${userID}/toReview`)
-      .set(toReviewClone);
   });
-
-  resp.send("Done refreshing toReview list!");
 });
+
+// exports.assignReviews = functions.https.onRequest(async (req, resp) => {
+//   const endDate = new Date();
+//   const startDate = new Date().setDate(endDate.getDate() - 14); // two weeks ago
+
+//   const items = await getItems();
+//   const users = await firebase
+//     .database()
+//     .ref("users")
+//     .once("value");
+
+//   await users.forEach(async snapshot => {
+//     const userID = snapshot.key;
+//     const toReview = snapshot.child("toReview");
+//     let toReviewClone = { ...toReview.val() };
+
+//     toReview.forEach(snap => {
+//       const id = snap.key;
+//       const val = snap.val();
+//       const itemStartDate = items[id] && items[id].dateStart;
+
+//       // Only consider items which are in the last 2 weeks
+//       if (
+//         itemStartDate &&
+//         itemStartDate > startDate &&
+//         itemStartDate <= endDate
+//       ) {
+//         toReviewClone[id] =
+//           toReview.child(id).val() == "not_assigned"
+//             ? "assigned"
+//             : toReviewClone[id];
+//         // eslint-disable-next-line
+//         console.log(`Assigned: ${id} for ${userID}`);
+//       } else {
+//         toReviewClone[id] =
+//           toReview.child(id).val() == "assigned"
+//             ? "not_assigned"
+//             : toReviewClone[id];
+//         // eslint-disable-next-line
+//         console.log(`Unassigned: ${id} for ${userID}`);
+//       }
+//     });
+
+//     await firebase
+//       .database()
+//       .ref(`users/${userID}/toReview`)
+//       .set(toReviewClone);
+//   });
+
+//   resp.send("Done refreshing toReview list!");
+// });
